086b6baef4c58f7515f12ee82c2c829cc23639e0
muramatsu.hiroshi <muramatsu.hiroshi@dicjapan.co.jp>
2024-12-10 23:43:25 +0000

    フォルダ構成変更に伴うpageのインポートの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui_elements/button";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+} from "@/components/ui_elements/accordion";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui_elements/alert";
 
 export default function Home() {
   return (
